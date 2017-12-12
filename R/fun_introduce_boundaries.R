dcff81b5b384e839a89d91bac90ff1fa22073266
ECSchmitt <33685589+ECSchmitt@users.noreply.github.com>
2017-12-12 14:55:14 +0100

    switched to a more intuitive variable name and added a comment on the warnings that occur while running this code

--- R/fun_introduce_boundaries.R
+++ R/fun_introduce_boundaries.R
@@ -1,12 +1,17 @@
 #builds a GRanges object containing boundaries by fixing the end coordinate of a given GRanges
 #object as center and expanding the sequence to both sides to a given size
 
-introduce_boundaries <- function(bedfile, size, seqinfo){
+introduce_boundaries <- function(bedfilepath, size, seqinfo){
 
-granges <- import(bedfile)      
+granges <- import(bedfilepath)      
 granges <- as_tibble(granges)
 granges <- GRanges (seqnames = granges$seqnames,IRanges(start = granges$end,end = granges$end),
                     seqinfo = hum_seqinfo)
+
+#produces 6 out of bound ranges at chromsome ends of chr7,10,11,15,19,21. Therefore these
+#boundaries are samller than 40k bp. Since they map at chromosome ends this is not
+#considered to introduce a major problem when analysing genepairs separated by a boundary
+#because only genes that are located on the same chromosome can form a genepair
 granges <- granges %>% 
   resize(fix = "center", width = size) %>% 
   trim()
