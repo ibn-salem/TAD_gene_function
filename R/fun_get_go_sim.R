91a9e9de1a006ed4e57f026e6f38a8cd0aabfe31
ECSchmitt <33685589+ECSchmitt@users.noreply.github.com>
2017-12-13 16:50:22 +0100

    changed map2 to map2_dbl

--- R/fun_get_go_sim.R
+++ R/fun_get_go_sim.R
@@ -1,10 +1,18 @@
+
 get_go_sim <- function (cispair, semdata){
   
-  map2(cispair$g1_id,
-       cispair$g2_id,
-       geneSim,
-       semData = semdata,
-       measure = "Resnik",
-       combine = "max")
+   map2_dbl(cispair$g1_id,
+            cispair$g2_id,
+            geneSim,
+            semData = semdata,
+            measure = "Resnik",
+            combine = "max")
   
 }
+
+
+
+
+
+
+
